df456c15df5a22c080d780e7e50d87bc00bd4c9d
ManoloEsS <manolo.estrada.s@gmail.com>
2025-07-26 01:16:18 -0600

    fixed cli-interface and moved it to the correct file

---
+++ cli-password-manager/cli_interface.py
@@ -0,0 +1,50 @@
+import argparse
+import functions
+
+
+def parser():
+    parser = argparse.ArgumentParser(
+        prog="clipm", description="a simple and secure password manager"
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    group.add_argument(
+        "-a",
+        "--add",
+        help="will ask you for your username and password that goes with it",
+        type=str,
+    )
+    group.add_argument(
+        "-r",
+        "--retrieve",
+        help="will retrieve your password for the specified username or service",
+        type=str,
+    )
+    group.add_argument(
+        "-l",
+        "--list",
+        help="will list out all services and usernames",
+        type=str,
+    )
+    group.add_argument(
+        "-m",
+        "--modify",
+        help="Modify an existing password or delete a saved username password pair",
+        type=str,
+    )
+    # group.add_argument(
+    #     "-g",
+    #     "--generate",
+    #     help="will generate a password, then will ask for comfirmation of that password. Then will ask the service and username the password would be saved under",
+    #     type=str,
+    # )
+    args = parser.parse_args()
+
+    if args.add:
+        functions.add_password()
+    elif args.retrieve:
+        functions.retrieve_password()
+    elif args.list:
+        functions.list_all()
+    elif args.modify:
+        functions.modify_remove_username()
--- cli-password-manager/main.py
+++ cli-password-manager/main.py
@@ -1,42 +1,11 @@
-import argparse
-import getpass
-import sys
+import cli_interface
+import functions
+import encryption
+
 
 def main():
-    print("Hello from cli-password-manager!")
-    parser = argparse.ArgumentParser(
-        prog="password manager",
-        description="a simple and secure password manager"
-    )
+    cli_interface.parser()
 
-    parser.add_argument(
-        "-a","--add",
-        help="will ask you for your username and password that goes with it",
-        type=str
-    )
-    parser.add_argument(
-        "-r","--retrieve",
-        help="will retrieve your password for the specified username or service",
-        type=str
-    )
-    parser.add_argument(
-        "-l","--list",
-        help="will list out all services and usernames",
-        type=str
-    )
-    parser.add_argument(
-        "-g","--generate",
-        help="will generate a password, then will ask for comfirmation of that password. Then will ask the service and username the password would be saved under",
-        type=str
-    )
-    #parser.add_argument(
-    #   "--resetall",
-    #    help="will reset your 4 encryption words but will delete all passwords in the process",
-    #)
-    args = parser.parse_args()
-
-    #TEST PRINT DELETE BEFORE SHIPPING
-    print("this works fine")
 
 if __name__ == "__main__":
     main()
