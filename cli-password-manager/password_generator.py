d8a62abe02cc39cb2f09f5d4afb23fb464b880ef
SoullessGent <108738982+SoullessGent@users.noreply.github.com>
2025-07-26 15:49:29 -0500

    Create password_generator.py

---
+++ cli-password-manager/password_generator.py
@@ -0,0 +1,24 @@
+import random
+
+
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+           'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+           'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+special_characters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+
+total_list = letters + numbers + special_characters
+def generate_password():
+    password = ""
+    for i in range(17):
+        randomchar = random.choice(total_list)
+        password += randomchar
+    return password
+
+    
+password = generate_password()
+print("Generated Password:", password)
